07f224c3e371978351642b972594d8415fffff65
Zmyhh <897268593@QQ.com>
2019-04-03 19:23:17 +0800

    node骏然，user表增删改查

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ let express = require('express')
 let router = express.Router()
 const mysql = require('mysql')
 const utility = require('utility')
-const sd=require('silly-datetime')
+const sd = require('silly-datetime')
 
 // let bodyParser=require('body-parser')
 //
@@ -12,7 +12,7 @@ const multer = require('multer')
 const fs = require('fs');
 const pathLib = require('path');
 //设置文件上传路径
-var upload = multer({dest: 'upload/'});
+var upload = multer({dest: 'D:/phpStudy/WWW/Image'});
 
 
 // 单图上传
@@ -20,9 +20,12 @@ router.post('/upload', upload.single('logo'), function (req, res, next) {
     var newName = req.file.path + pathLib.parse(req.file.originalname).ext;
     fs.rename(req.file.path, newName, function (err) {
         if (err) {
+            // filename
             return res.status(200).json({code: 1, message: '添加失败！'})
         } else {
-            return res.status(200).json({code: 1, message: '添加成功！'})
+            let imgMint=req.file.originalname.substr(req.file.originalname.indexOf('.'))
+            console.log(imgMint)
+            return res.status(200).json({code: 1, message: '添加成功！',url:`http://192.168.1.179:/Image/${req.file.filename}${imgMint}`})
         }
         res.end();
     })
@@ -59,10 +62,11 @@ function select(key) {
     return p
 
 }
+
 /**
  * 查询
  * **/
-router.get('/query',((req,res)=>{
+router.get('/query', ((req, res) => {
     console.log('分页查询')
     const connection = mysql.createConnection({
         host: 'localhost',
@@ -71,24 +75,24 @@ router.get('/query',((req,res)=>{
         database: 'nodejs',
     })
 
-    let pageSize=req.query.pageSize
-    let pageNum=(Number(req.query.pageNum)-1)*pageSize  //查询页数（偏移量）
+    let pageSize = req.query.pageSize
+    let pageNum = (Number(req.query.pageNum) - 1) * pageSize  //查询页数（偏移量）
     console.log(req.query)
     console.log(pageNum)
     let num
     //查询总数量
     connection.query(`SELECT SQL_CALC_FOUND_ROWS * from user`, (err, results, fields) => {
-        num=results.length
+        num = results.length
         connection.end()
     })
     connection.query(`SELECT * from user limit ${pageNum},${pageSize}`, (err, results, fields) => {
-        return res.status(200).json({code: 1,message: 'ojbk',data:{count:num,dataList:results}})
+        return res.status(200).json({code: 1, message: 'ojbk', data: {count: num, dataList: results}})
         connection.end()
     })
-        // connection.query(`SELECT SQL_CALC_FOUND_ROWS * from user limit 0,10`, (err, results, fields) => {
-        //     return res.status(200).json({code: 1,torlent:results.length,aaa:fields,message: results})
-        //     connection.end()
-        // })
+    // connection.query(`SELECT SQL_CALC_FOUND_ROWS * from user limit 0,10`, (err, results, fields) => {
+    //     return res.status(200).json({code: 1,torlent:results.length,aaa:fields,message: results})
+    //     connection.end()
+    // })
 
 
 }))
@@ -134,10 +138,10 @@ router.post('/add', ((req, res) => {
 
     })
     console.log(req.body)
-    let time=sd.format(new Date(),"YYMDHms")
+    let time = sd.format(new Date(), "YYMDHms")
     console.log(time)
     connection.connect();//连接数据库
-    connection.query(`insert into user values('${time}','${req.body.username}','${req.body.password}')`, (err, results) => {
+    connection.query(`insert into user values('${time}','${req.body.username}','${utility.md5(req.body.password)}')`, (err, results) => {
         // '3','${req.body.username}','${req.body.password}
         if (err) {
             return res.status(200).json({code: 1, message: `添加失败${err}`})
@@ -198,5 +202,83 @@ router.post('/add', ((req, res) => {
 
 }))
 
+
+/**
+ *
+ * 修改
+ * **/
+router.post('/updata', ((req, res) => {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '123456',
+        database: 'nodejs',
+    })
+    let pass = req.body.password
+    let ID = req.body.userID
+
+    if (pass && ID) {
+        connection.connect();//链接数据库
+        connection.query(`UPDATE user SET password=${req.body.password} where userID=${req.body.userID}`, (error, result) => {
+            if (error) {
+                return res.status(200).json({code: 500, message: `${error}`})
+            } else if (result) {
+                if (result.changedRows == 1) {
+                    return res.status(200).json({code: 200, message: '修改成功', data: result})
+                } else {
+                    return res.status(200).json({code: 200, message: '修改失败'})
+                }
+            } else {
+                return res.status(200).json({code: 500, message: `网络错误`})
+            }
+
+        })
+
+        connection.end()
+    } else {
+        return res.status(200).json({code: 500, message: `参数不全`})
+    }
+
+
+}))
+
+/***
+ * 删除
+ * **/
+router.post('/delete', ((req, res) => {
+    // DELETE FROM user WHERE userID in ("2","3");
+    //
+
+    let id = req.body.ids
+    let ids = eval(id) //将字符串类型转换为数组
+    let parameters = ''
+
+    ids.forEach(item => {
+        parameters += `${item},`
+    })
+    parameters = parameters.substring(0, parameters.length - 1);//截取指定字符串
+
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '123456',
+        database: 'nodejs'
+    })
+    connection.connect(),//连接数据库
+        connection.query(`delete from user where userID in(${parameters})`, (error, result) => {
+            if (error) {
+                return res.status(200).json({code: 500, message: `${error}`})
+            } else if (result) {
+                if (result.affectedRows != 0) {
+                    return res.status(200).json({code: 200, message: `成功删除${result.affectedRows}条记录`, data: result})
+                } else {
+                    return res.status(200).json({code: 200, message: '删除失败'})
+                }
+
+            }
+        })
+
+
+}))
 
 module.exports = router;
